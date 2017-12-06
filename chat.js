957c26bff84937e7b010ff7a7325d02dfb4ebe4c
Maxime <mmaaxxboumma@gmail.com>
2017-12-06 22:24:18 +0100

    yop

--- chat.js
+++ chat.js
@@ -13,16 +13,27 @@ console.log('YOP!');
 
 app.get('/', function(req, res){
 	//res.setHeader('Content-Type', 'text/plain');
-	res.render('chat.ejs', 0);
+	res.render('hello.ejs');
+})
+.get('/:ibRoom', function(req, res){
+    res.render('chat.ejs');
 });
 
 wss.on('connection', function connection(ws){
-	var msgConnexion = {
-        msg: "Bienvenue sur le Chat!",
-        pseudo: "SERVEUR"
-    }
-    ws.send(JSON.stringify(msgConnexion));
+    wss.client[wss.clients.lenght - 1].test = "yop";
 	ws.on('message', function(msg){
+        
+        var data = JSON.parse(msg);
+        if(data.connect !== undefined && data.connect == 'first')
+        {
+            var msgConnexion = {
+                msg: "Bienvenue sur le Chat!",
+                pseudo: "SERVEUR"
+            }
+            ws.send(JSON.stringify(msgConnexion));
+            console.log(ws.client.test);
+        }
+        
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(msg);
